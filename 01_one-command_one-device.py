a300ca9aa780c81d2d8ad9f7628a74acc9ee4c15
Gandalf <vodovilich@gmail.com>
2023-04-29 06:22:47 -0400

    rename file

--- 01_one-command_one-device.py
+++ 01_one-command_one-device.py
@@ -1,19 +1,27 @@
 import pexpect
 import sys
 
+
+#pexpect defaults: 
+# .expect returns 0 if matches the argument
+# .sendline returns number of bytes send (1 symbol = 1 byte) + 1 byte for NEWLINE symbol
 print("pidor")
 
 child = pexpect.spawn('ssh gandalf@192.168.100.27')
 child.logfile = sys.stdout.buffer
 child.expect('assword')
 child.sendline('grey')
+
 child.expect('#')
 child.sendline('terminal length 0')
+
 child.expect('#')
 child.sendline('show ver')
+
 child.expect('#')
 x = child.before.decode('utf-8').splitlines()
 child.sendline('exit')
 
 for line in x:
     print(line)
+
